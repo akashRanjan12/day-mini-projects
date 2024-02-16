07066bbdb12a7f0ee22cf19ab40d3457aedb3be3
akashRaanjan12 <akash12ranjan@gmail.com>
2024-02-16 12:15:18 +0530

    loader

---
+++ index.js
@@ -0,0 +1,4 @@
+let fullname = prompt("enter your full name");
+console.log(`@${fullname}` + fullname.length);
+// alert("you have successfull entered your name");
+confirm("do you want to continue?");
