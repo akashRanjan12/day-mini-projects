6df8a0da4630a7d90c3953f7107727777330a1e0
akashRaanjan12 <akash12ranjan@gmail.com>
2024-02-17 09:33:57 +0530

    Add:button_03

--- index.js
+++
@@ -1,4 +0,0 @@
-let fullname = prompt("enter your full name");
-console.log(`@${fullname}` + fullname.length);
-// alert("you have successfull entered your name");
-confirm("do you want to continue?");
